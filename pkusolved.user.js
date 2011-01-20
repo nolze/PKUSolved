635d90059a435a00dbbcc96656141cb00f0a284c
nolze <nolze@archlinux.us>
2011-01-20 16:44:47 +0900

    fixed a parsing problem

--- pkusolved.user.js
+++ pkusolved.user.js
@@ -10,18 +10,18 @@
 		id = window.prompt("Enter your user ID", null);
 		if(id){ window.localStorage.setItem("id", id); }
 	}
-	s = window.localStorage.getItem("solved");
-	if(s){ s = JSON.parse(s); }
 	var chk = function(obj){
 		s = new Array();
 		s.push(new Date().getTime()+24*60*60*1000);
 		s = s.concat(obj.responseText.match(/p\(....\)/g));
 		window.localStorage.setItem("solved", JSON.stringify(s));
 	};
+	s = window.localStorage.getItem("solved");
 	if(!s||new Date().getTime()>s[0]){
 		GM_xmlhttpRequest({method:"get", url:"http://poj.org/userstatus?user_id="+id, onload:chk});
 		s = window.localStorage.getItem["solved"];
 	}
+	s = JSON.parse(s);
 	if(s.indexOf("p("+document.title.substr(0,4)+")")!=-1){
 		document.title = "[S]" + document.title;
 	}
